0a96a165ea7058a9a119dffcc14a4e69f9cba955
Diego-Valdiviezo-S <109091399+Diego-Valdiviezo-S@users.noreply.github.com>
2022-07-22 15:47:13 -0600

    Add files via upload

---
+++ Excepciones/ConsoleApp3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+        public void Fail()
+            {
+
+            throw new NotImplementedException(message: "Error!");
+     
+        }
+    }
+}
